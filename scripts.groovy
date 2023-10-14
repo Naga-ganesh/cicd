c7b568b0f85de10e4ac1c3a3b22d9fce214c45b4
ganesh <nagaganesh3108@gmail.com>
2023-10-14 21:30:40 +0530

    changes made

---
+++ scripts.groovy
@@ -0,0 +1,10 @@
+def build() {
+
+    echo "Building docker container"
+    echo "tagging docker image"
+    echo "Current branch name is the ${BRANCH_NAME}" 
+    echo "build name: ${BUILD_NAME}"
+    echo "${GIT_KEY}"
+    echo "Feature name ${params.FEATURE_NAME} with the version of ${params.version}"
+    echo "${TEST_PASS}"
+}
